d0ab81fc267896b260432dffde89b423472ea9ed
Aaron Phan <aarn.phan@gmail.com>
2023-02-06 17:20:12 +1100

    Finish project 2

--- HWS-P2-Country-Flag-Game-App/Controller/ViewController.swift
+++ HWS-P2-Country-Flag-Game-App/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
     
     @IBOutlet var button1: UIButton!
@@ -30,17 +31,35 @@ class ViewController: UIViewController {
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
+        
+        correctAnswer = Int.random(in: 0...2)
         askQuestion()
+        title = countries[correctAnswer].uppercased()
         
 
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        title = countries[correctAnswer].uppercased()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
 }
 
